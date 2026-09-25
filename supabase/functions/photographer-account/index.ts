4f896ac2a294bde286d3a17a4dea6c72c95d8ee8
NguyenAnh <114822159+Quanhcutephomaiquehatme@users.noreply.github.com>
2026-09-26 02:03:30 +0700

    Update index.ts

--- supabase/functions/photographer-account/index.ts
+++ supabase/functions/photographer-account/index.ts
@@ -1,3 +1,6 @@
-import { createClient } from 'npm:@supabase/supabase-js@2';
-import { handle } from './handler.js';
-Deno.serve((request: Request) => handle(request, Deno.env, createClient));
+import { createClient } from "npm:@supabase/supabase-js@2";
+import { handle } from "./handler.js";
+
+Deno.serve((request: Request) => {
+  return handle(request, Deno.env, createClient);
+});
